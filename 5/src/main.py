dec389bfae1a42db6a3f3dd3899b1aa1556d68f7
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:58:46 +0300

    Update main.py

--- 5/src/main.py
+++ 5/src/main.py
@@ -4,5 +4,7 @@ from typing import Optional
 app = FastAPI()
 
 # BEGIN (write your solution here)
-
+@app.post("/users")
+def create_user(username: str = Body(...), email: str = Body(...), age: Optional[int] = Body(None)):
+    return {"username": username, "email": email, "age": age, "status": "User created"}
 # END
