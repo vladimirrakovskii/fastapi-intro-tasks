7616a5ec039c160b7c807dbfd6d67649eabc4412
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:57:00 +0300

    Update main.py

--- 2/src/main.py
+++ 2/src/main.py
@@ -3,5 +3,9 @@ from fastapi import FastAPI, Query
 app = FastAPI()
 
 # BEGIN (write your solution here)
-
+@app.get("/filter")
+def min_max(
+        min: int = Query(default=0, gt=0),
+        max: int = Query(default=100, lt=100)):
+    return {"min": min, "max": max}
 # END
