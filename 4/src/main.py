5912115b96f52b4c2a8bfd0dd41a0f2dc2911d6f
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:58:12 +0300

    Update main.py

--- 4/src/main.py
+++ 4/src/main.py
@@ -3,5 +3,9 @@ from fastapi import FastAPI, Cookie
 app = FastAPI()
 
 # BEGIN (write your solution here)
-
+@app.get("/language")
+def lang(language: str = Cookie(default=None)):
+    if language != None:
+        return {"language": language}
+    return {"message": "Language not set"}
 # END
