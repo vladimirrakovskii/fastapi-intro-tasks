39e02779d9cc593ad860f7711143f1026ee917c3
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:59:17 +0300

    Update main.py

--- 6/src/main.py
+++ 6/src/main.py
@@ -3,5 +3,7 @@ from fastapi import FastAPI, Form
 app = FastAPI()
 
 # BEGIN (write your solution here)
-
+@app.post("/login")
+def user_login(username: str = Form(...), password: str = Form(...)):
+    return {"username": username, "password": password, "status": "Login successful"}
 # END
