c36c6eeef7d7ad01533426c384e3aee941f656be
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:57:39 +0300

    Update main.py

--- 3/src/main.py
+++ 3/src/main.py
@@ -3,5 +3,7 @@ from fastapi import FastAPI, Path
 app = FastAPI()
 
 # BEGIN (write your solution here)
-
+@app.get("/users/{user_id}")
+def id_path(user_id: int = Path(gt=0)):
+    return {"user_id": user_id}
 # END
