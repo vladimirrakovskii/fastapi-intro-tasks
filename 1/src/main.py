ad98f883141b4d549778d05638f8c83867878f2c
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:56:05 +0300

    Update main.py

--- 1/src/main.py
+++ 1/src/main.py
@@ -3,5 +3,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # BEGIN (write your solution here)
-
+@app.get("/reverse/{text}")
+def reverse_text(text):
+    reversed_text = ''.join(reversed(text))
+    return {"reversed": f"{reversed_text}"}
 # END
